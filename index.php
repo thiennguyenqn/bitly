7bb1f33b92f87fae925e1760ecb45332db984877
thiennguyenqn <44944761+thiennguyenqn@users.noreply.github.com>
2019-04-07 12:58:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
-# $url = $_POST('link');
-# Shorten Bitly
-$url = "url" . $_GET['url'];
-$tokenBitly = "40a92589f1e3c2dba496995fe8291d7ae0226f27"; //Nhập Access Token
-$get = file_get_contents("https://api-ssl.bitly.com/v3/shorten?access_token=".$tokenBitly."&longUrl=".$url);
+if(!empty($_GET['url']))
+{
+$url = $_GET['url'];
+$token = "40a92589f1e3c2dba496995fe8291d7ae0226f27"; //Nhập Access Token
+$get = file_get_contents("https://api-ssl.bitly.com/v3/shorten?access_token=".$token."&longUrl=".$url);
 $decode = json_decode($get);
-$shortURL = $decode->data->url;
-//echo $shortURL;
-echo '{"messages": [{"text": "' . $shortURL . '"}]}'; // for chatbot
-?>
+$short = $decode->data->url;
+//echo $short;
+echo '{"messages": [{"text": "' . $short . '"}]}'; // for chatbot
+}
+else
+{
+echo '{"messages": [{"text": "Hãy nhập url"}]}';;
+}
