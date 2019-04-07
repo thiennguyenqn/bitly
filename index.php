d3c21295582cbc21d5b2a68910dfaafe60a68fdf
thiennguyenqn <44944761+thiennguyenqn@users.noreply.github.com>
2019-04-07 12:36:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,5 +7,5 @@ $get = file_get_contents("https://api-ssl.bitly.com/v3/shorten?access_token=".$t
 $decode = json_decode($get);
 $shortURL = $decode->data->url;
 //echo $shortURL;
-echo '{"messages": [{"text": "' . $shortURL . '"}]}';
+echo '{"messages": [{"text": "' . $shortURL . '"}]}'; // for chatbot
 ?>
